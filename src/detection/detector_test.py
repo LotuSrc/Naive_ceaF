f58d1519f4040bb1c2b9c94b95e15fb973ad43d1
LotuSrc <451415738@qq.com>
2018-10-21 15:39:08 +0800

    add config

--- src/detection/detector_test.py
+++ src/detection/detector_test.py
@@ -1,3 +1,27 @@
+<<<<<<< HEAD
+# -*- coding: utf-8 -*-
+# @Time    : 2018/10/21 14:34
+# @Author  : Ruichen Shao
+# @File    : detector_test.py
+
+from src.detection.cv_detector import OpencvDector
+import cv2
+from src.detection.cv_config import Config
+
+image_path = Config.ROOT + 'resources/face_image/1.jpg'
+image = cv2.imread(image_path)
+
+# OpencvDector 单元测试
+opencv_detector = OpencvDector()
+opencv_faces = opencv_detector.detect(image_path)
+
+for (x, y, w, h) in opencv_faces:
+    image = cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+cv2.imshow('opencv_image', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+=======
 # coding=utf-8
 
 from src.detection.face_reg_detector import FaceRegDetector
@@ -17,3 +41,4 @@ def test():
 # wd = os.chdir
 
 test()
+>>>>>>> 41694cca75b132074bba423451ace0d0cf59063e
