3aeeeebae2cea4753538d743a92e2b5721f79489
EricFH <fanghao_zju@163.com>
2018-10-21 19:46:57 +0800

    add main as endpoint

---
+++ main.py
@@ -0,0 +1,5 @@
+file_path = "src/detection/face_reg_detector_test.py"
+
+with open(file_path) as f:
+    code = f.read()
+exec(code)
--- src/detection/face_reg_detector.py
+++ src/detection/face_reg_detector.py
@@ -9,6 +9,6 @@ class FaceRegDetector(BaseDetetctor):
     def detect(self, image_path):
         image = face_recognition.load_image_file(image_path)
         # 这里的model可以设置成cnn，会有更高的准确率，但需要GPU环境
-        face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=2, model='cnn')
+        face_locations = face_recognition.face_locations(image, model='cnn')
         face_locations = [(left, top, right - left, bottom - top) for (top, right, bottom, left) in face_locations]
         return face_locations
